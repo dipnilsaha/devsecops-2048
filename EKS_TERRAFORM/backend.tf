3fd9e5826dcb57b7a1d60ba429a9e9fe9b56d838
arnabroychoudhury <29061477+arnabroychoudhury@users.noreply.github.com>
2026-06-07 16:02:15 +0100

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "devsecops-2048-arnab-4" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "eu-west-2"
   }
 }
